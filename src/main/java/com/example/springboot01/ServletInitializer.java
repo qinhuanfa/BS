f91f4eafb4cf644df20b1ecee9aaa45f525f6630
Qin <2102651406@qq.com>
2020-09-15 17:29:03 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot01/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springboot01;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Springboot01Application.class);
+	}
+
+}
---
+++ src/test/java/com/example/springboot01/Springboot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
